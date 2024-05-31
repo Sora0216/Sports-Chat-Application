cb0c919e0fefb41e85e68e4e1dfcfd1b9acb6ec5
tcarocci <t.23carocci@yahoo.com>
2024-05-30 20:38:23 -0500

    fix registration and login issues, update handlebar files and routes

---
+++ routes/chatRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+
+const isAuthenticated = (req, res, next) => {
+  if (req.session.userId) {
+    return next();
+  }
+  res.redirect('/auth/login');
+};
+
+
+router.get('/', isAuthenticated, (req, res) => {
+  res.render('chat', { username: req.session.username });
+});
+
+module.exports = router;
